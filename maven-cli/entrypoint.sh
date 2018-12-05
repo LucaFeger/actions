5551dd6239c437ffecd2901f63cfc35c7d51a6c0
Luca Feger <luca@lucafeger.de>
2018-12-05 20:04:03 +0100

    Added docker-deplay Action

--- maven-cli/entrypoint.sh
+++ maven-cli/entrypoint.sh
@@ -1,7 +1,34 @@
-#!/bin/sh -l
+#!/bin/sh
 
-echo "==========Starting Maven Commands=========="
+# Stop after non zero exit code
+set -e
 
-sh -c "mvn $*"
+SSH_PATH="/github/home/.ssh"
 
-echo "==========Finished Maven Commands=========="
+mkdir "$SSH_PATH"
+touch "$SSH_PATH/known_hosts"
+
+echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
+echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
+
+chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/known_hosts"
+chmod 600 "$SSH_PATH/deploy_key"
+chmod 600 "$SSH_PATH/deploy_key.pub"
+
+eval $(ssh-agent)
+ssh-add "$SSH_PATH/deploy_key"
+
+ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
+
+ssh -A -tt -o 'StrictHostKeyChecking=no' $USER@$HOST
+
+docker stop $(docker ps -a -q  --filter ancestor=$1)
+docker rm $(docker ps -a -q  --filter ancestor=$1)
+docker rmi $1
+
+if [ -z "$DOCKER_PARAMETERS" ]; then
+    docker run -d $1;
+else
+    docker run $DOCKER_PARAMETERS -d $1;
+fi
